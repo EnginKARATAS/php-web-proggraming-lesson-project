2e3fede00c9e37017c006bc79e83757851afcfec
Engin Karataş <enginkaratas99@gmail.com>
2021-06-25 01:44:52 +0300

    update

--- php/bilgigetir.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "db_config.php";
-$id = 1;
-$stmt = $db_con->prepare("SELECT * FROM homeview WHERE ID=:id"); 
-$stmt->execute(array(':id'=>$id));
-$row=$stmt->fetch(PDO::FETCH_ASSOC);
-?>
